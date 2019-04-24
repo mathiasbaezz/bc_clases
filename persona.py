bd062db8c54ebc01d99f0be1d77f2e23b8c1a1a3
Mathias Baez <mathiasbaezz@gmail.com>
2019-04-25 00:07:21 +0200

    edad de persona

--- persona.py
+++ persona.py
@@ -1,6 +1,15 @@
 class Persona:
     nombre = None
-    def __init__(self,el_nombre):
+    edad = None
+    def __init__(self,el_nombre,la_edad):
         self.nombre=el_nombre
-        print("Hola me llamo",self.nombre)
-name = Persona("Maa")
+        self.edad = la_edad
+        print("Hola me llamo",self.nombre,"tengo",self.edad)
+
+    def get_edad(self):
+        return self.edad
+    
+    def set_edad(self, cantidad):
+        self.edad=cantidad
+        
+name = Persona("Mathias",20)
